69ac863fca0da90c570e71e7a63bbcb150c756dc
Finnlou13 <gerlach.finn.07@gmail.com>
2026-03-09 10:59:08 +0100

    feature: added Settings class

---
+++ app/src/main/java/com/example/absenceviewer/Settings.kt
@@ -0,0 +1,9 @@
+package com.example.absenceviewer
+
+
+//TODO: implement settings
+class Settings {
+    public val colorTheme = 0
+    public val studentClass = 0
+    public val notifications = false
+}
